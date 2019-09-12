dc89007fe76c3b58913049fc91311642220484b4
Andrea McGrane <andrea.mcgrane@gmail.com>
2019-09-12 17:05:23 -0500

    Added documentation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
-
+/**
+ * Author: Andrea McGrane and Gracyn Green
+ * Date: 09/12/2019
+ * This is a hello world project that prints both of our names
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
